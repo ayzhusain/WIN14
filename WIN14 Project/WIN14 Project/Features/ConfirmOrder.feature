ba8af0ad002076c6440ce6cd5c28c9ab34ac8ec4
Fredrik Samuelsson <fredrik319@gmail.com>
2015-12-21 11:42:52 +0100

    Create ConfirmOrder feature

---
+++ WIN14 Project/WIN14 Project/Features/ConfirmOrder.feature
@@ -0,0 +1,7 @@
+﻿Feature: ConfirmOrder
+
+@mytag
+Scenario: Confirm the order
+	Given I stand on checkout page
+	When I press Confirm order
+	Then Show Thank You message
---
+++ WIN14 Project/WIN14 Project/Features/ConfirmOrderSteps.cs
@@ -0,0 +1,33 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Firefox;
+using System;
+using TechTalk.SpecFlow;
+
+namespace WIN14_Project.Features
+{
+    [Binding]
+    public class ConfirmOrderSteps
+    {
+        IWebDriver driver = null;
+
+        [Given(@"I stand on checkout page")]
+        public void GivenIStandOnCheckoutPage()
+        {
+            driver = new FirefoxDriver();
+            driver.Navigate().GoToUrl("http://localhost:2110/Html/ConfirmOrder.html");
+        }
+        
+        [When(@"I press Confirm order")]
+        public void WhenIPressConfirmOrder()
+        {
+            driver.FindElement(By.Id("btnConfirm")).Click();
+        }
+        
+        [Then(@"Show Thank You message")]
+        public void ThenShowThankYouMessage()
+        {
+            Assert.True(driver.Title == "Confirm Order", "Order Confirmed");
+        }
+    }
+}
