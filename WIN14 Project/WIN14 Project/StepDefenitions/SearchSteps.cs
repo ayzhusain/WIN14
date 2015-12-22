2759b3ada452e73a7b1701038e37e6be044b2a34
Teddymannen <teddy_kross@hotmail.com>
2015-12-22 10:22:46 +0100

    Adding Step definition for Search

---
+++ WIN14 Project/WIN14 Project/StepDefenitions/SearchSteps.cs
@@ -0,0 +1,33 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Firefox;
+using System;
+using TechTalk.SpecFlow;
+
+namespace WIN14_Project.StepDefenitions
+{
+    [Binding]
+    public class SearchSteps
+    {
+        IWebDriver driver = null;
+
+        [Given(@"You are at product page")]
+        public void GivenYouAreAtProductPage()
+        {
+            driver = new FirefoxDriver();
+            driver.Navigate().GoToUrl("https://localhost:2110/html/startPage.html");
+        }
+        
+        [When(@"You search for product")]
+        public void WhenYouSearchForProduct()
+        {
+            driver.FindElement(By.Id("search")).SendKeys("Tandpetare");
+            driver.FindElement(By.Id("searchButton")).Click();
+        }
+        
+        [Then(@"See product")]
+        public void ThenSeeProduct()
+        {
+            driver.FindElement(By.Id("clickThis")).Click();
+        }
+    }
+}
